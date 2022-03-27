8f5bfcc1bb9f6c7c7ed9d5a73c662410ae8e8e83
tripa <tripa@DESKTOP-NBMQD03>
2022-03-27 14:25:05 +0530

    Learning Java

---
+++ Learning Java/src/Unit_01/P1_Task01.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package Unit_01;
+
+/**
+ * @author deepc
+ *
+ */
+public class P1_Task01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello this is my 1st program");
+
+	}
+
+}
